6ca9d888e5e72cb472ec62873a3f6f0549ba3012
HrishikeshGirsawale <137191802+HrishikeshGirsawale@users.noreply.github.com>
2024-11-22 23:07:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,5 +19,5 @@ with st.expander('Data'):
   y
 
 with st.expander('Data visualization'):
-  st.scatter_chart(data=df, x='temperature', y='humidity')
+  st.scatter_chart(data=df, x='temparature', y='humidity')
 
