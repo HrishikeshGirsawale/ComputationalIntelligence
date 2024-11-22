70ffdb45eaea7ec6212bdeca5c9936baa447cbcc
HrishikeshGirsawale <137191802+HrishikeshGirsawale@users.noreply.github.com>
2024-11-22 22:48:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Rainfall Prediction')
 
 st.info('This is a rainfall prediction app')
+
+df = pd.read_csv('https://raw.githubusercontent.com/HrishikeshGirsawale/duniya_ka_papa/refs/heads/main/Rainfall.csv')
+df
